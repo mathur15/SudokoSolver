9eb12bd74ff2e22a16b246c490a213f4e39ce57d
mathur15 <som.mathur1997@gmail.com>
2020-04-24 19:55:31 -0400

    Set up grid, need to link to logic

---
+++ UI_grid.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+main = Tk()
+
+entries = []
+for a in range(9):
+    for b in range(9):
+        temp = Entry(main)
+        temp.grid(row=a, column=b, pady=2)
+
+
+main.mainloop()
